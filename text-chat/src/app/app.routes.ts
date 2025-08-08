a8baaeeef49ddbed274a0d09e8298d65357c81a4
BrandonHannan <brande654@gmail.com>
2025-08-08 12:44:15 +1000

    Added routing and created the navbar

--- text-chat/src/app/app.routes.ts
+++ text-chat/src/app/app.routes.ts
@@ -1,3 +1,27 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Home } from './home/home.component';
+import { Account } from './account/account.component';
+import { Groups } from './groups/groups.component';
+import { Register } from './register/register.component';
+import { Login } from './login/login.component';
+import { ChannelComponent } from './channel/channel.component';
+import { TextChat } from './text-chat/text-chat.component';
+import { Messages } from './messages/messages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: Login},
+    {path: 'register', component: Register},
+    {path: 'groups', component: Groups},
+    {path: 'channels', component: ChannelComponent},
+    {path: 'messages', component: Messages},
+    {path: '', component: TextChat},
+    {path: 'account', component: Account},
+    {path: '', component: Home}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- text-chat/src/app/app.ts
+++ text-chat/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { NotificationComponent } from './notification/notification.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterModule, NotificationComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('text-chat');
+  constructor(private router: Router) {}
 }
